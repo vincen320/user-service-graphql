f367ed4b82d6299bbcb114cf0e1b8ba25fe5fedf
Vincen <vincen320@gmail.com>
2023-04-15 01:27:25 +0700

    remove unncessary comment

--- main.go
+++ main.go
@@ -12,7 +12,4 @@ func main() {
 		panic(err)
 	}
 	cmd.Execute()
-	//BUAT TABLE BARU SBG RELASI DAN GET JGUA [DONE] (CHILDS)
-	//coba lihat dokumentasi yang operation name yang HeroAndFriends --> WILL DO (ongoing)
-	//BUAT UNTUK MUTATIONS (baca lagi dokumentasi)
 }
